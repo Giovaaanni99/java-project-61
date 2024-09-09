945de9ae3258133c66ca4304ab8796a56aede332
Giovaaanni99 <ivan.g92@mail.ru>
2024-09-10 00:12:02 +0300

    Step 4 has been completed.The repository is connected to Code Style. CheckStyle is configured in IDEA

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,7 +3,7 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void tallk() {
+    public static void talk() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Welcome to the Brain Games!\nMay I have your name? ");
